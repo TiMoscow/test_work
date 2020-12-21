a5624b9ca1b073bd609de383e0af56671f5c9949
TiMoscow <tim-kap@ya.ru>
2020-12-21 06:02:45 +0300

    test work 0.0.3 новые фичи

--- closed.php
+++ closed.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 echo "Этот файл недоступен для просмотра";
 
 $a[0] = 1;
@@ -13,8 +14,11 @@ $b[10] = 11;
 $result = count($b);
 // $result == 3;
 
-$result = count(null);
+$result1 = count(null);
 // $result == 0;
 
-$result = count(false);
+$result2 = count(false);
 // $result == 1;
+
+$_SESSION['message_no_good'] .= 'в разработке (пустышка) \n';
+header("Location: ".$_SERVER["HTTP_REFERER"]);
--- closed2.php
+++ closed2.php
@@ -2,6 +2,6 @@
 session_start();
 echo "Этот файл недоступен для просмотра";
 
-$_SESSION['message_no_good'] .= 'Не авторизован! \n';
+$_SESSION['message_no_good'] .= 'Не авторизован! (пустышка) \n';
 header("Location: ".$_SERVER["HTTP_REFERER"]);
 exit;
